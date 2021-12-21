f8c98f8e3d1e253a490d3001e0a28340182125de
Rémy El Sibaïe <remy@nomadic-labs.com>
2021-12-21 17:29:21 +0100

    day21

--- bin/advent.ml
+++ bin/advent.ml
@@ -1,5 +1,6 @@
 module Int = BatInt
 module IntSet = Set.Make (Int)
+module IntMap = Map.Make (Int)
 module CharSet = Set.Make (Char)
 module CharSetMap = Map.Make (CharSet)
 module FloatSet = Set.Make (Float)
@@ -76,3 +77,108 @@ let update_tbl h k default f =
     match Hashtbl.find h k with exception Not_found -> default | v -> f v
   in
   Hashtbl.replace h k v
+
+let memo_rec h f =
+  let rec g v =
+    match Hashtbl.find h v with
+    | exception Not_found ->
+        let r = f g v in
+        Hashtbl.add h v r;
+        r
+    | r -> r
+  in
+  g
+
+module Day21 = struct
+  let nroll_by_turn = 3
+
+  module Part1 = struct
+    let roll_dice dice =
+      let rec aux dice move nroll =
+        if nroll = nroll_by_turn then (move, dice)
+        else
+          let dice = dice + 1 in
+          let dice = if dice = 101 then 1 else dice in
+          aux dice (move + dice) (nroll + 1)
+      in
+      aux dice 0 0
+
+    let move pos nmove =
+      let move_mod = (nmove + pos) mod 10 in
+      if move_mod = 0 then 10 else move_mod
+
+    let play_turn total_roll dice (pos, score) =
+      let nmove, dice = roll_dice dice in
+      let pos = move pos nmove in
+      (total_roll + nroll_by_turn, dice, (pos, score + pos))
+
+    let play_game p1_pos p2_pos =
+      let rec aux dice total_roll p1 p2 =
+        if snd p1 >= 1000 then (total_roll, snd p2)
+        else if snd p2 >= 1000 then (total_roll, snd p1)
+        else
+          let total_roll, dice, p1 = play_turn total_roll dice p1 in
+          aux dice total_roll p2 p1
+      in
+      aux 0 0 (p1_pos, 0) (p2_pos, 0)
+  end
+
+  module Part2 = struct
+    let sums =
+      let a = ref IntMap.empty in
+      for i = 1 to 3 do
+        for j = 1 to 3 do
+          for k = 1 to 3 do
+            a :=
+              IntMap.update
+                (i + j + k)
+                (function Some prev -> Some (prev + 1) | None -> Some 1)
+                !a
+          done
+        done
+      done;
+      IntMap.bindings !a
+
+    let () =
+      Format.printf "%a@\n"
+        (printl (fun fmt (v, _) -> Format.fprintf fmt "%d " v))
+        sums
+
+    let play_turns (pos, score) =
+      List.map
+        (fun (nmove, mul) ->
+          let pos = Part1.move pos nmove in
+          ((pos, pos + score), mul))
+        sums
+
+    let play_game p1_pos p2_pos =
+      let aux aux (is_p1, p1, p2) =
+        if snd p1 >= 21 then if is_p1 then (1, 0) else (0, 1)
+        else if snd p2 >= 21 then if is_p1 then (0, 1) else (1, 0)
+        else
+          let p1_universes = play_turns p1 in
+          List.fold_left
+            (fun (p1win_acc, p2win_acc) (p1, mul) ->
+              let p1win, p2win = aux (not is_p1, p2, p1) in
+              ((p1win * mul) + p1win_acc, p2win + p2win_acc))
+            (0, 0) p1_universes
+      in
+      let aux = memo_rec (Hashtbl.create 17) aux in
+      let p1win, p2win = aux (true, (p1_pos, 0), (p2_pos, 0)) in
+      max p1win p2win
+  end
+
+  let run () =
+    let p1 = 10 in
+    let p2 = 9 in
+
+    let total_roll, loser_score = Part1.play_game p1 p2 in
+    Format.printf "%d * %d = %d @\n" total_roll loser_score
+      (total_roll * loser_score);
+
+    Format.printf "%d@\n" (Part2.play_game p1 p2);
+    ()
+  ;;
+
+  run ()
+end
